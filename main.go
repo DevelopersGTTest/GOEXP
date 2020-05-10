3aaa717bd92340b73b7ee1c36390b48094fe6937
p-jacobo2012240 <pablojacobo1998@gmail.com>
2020-05-09 23:12:42 -0600

    definitions

--- main.go
+++ main.go
@@ -3,8 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	n1, n2, n3 := 1, 2, 4
+	n1, n2, n3, sam := 1, 2, 4, "hola soy sam"
 	fmt.Println(n1)
 	fmt.Println(n2)
 	fmt.Println(n3)
+	fmt.Println(sam)
+	processLogic()
+}
+
+func processLogic() {
+	fmt.Println("processs...  ")
 }
